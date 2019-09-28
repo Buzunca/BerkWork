868d95f95b9d639daf52070ed9e12eadb2051174
Buzunca <55912356+Buzunca@users.noreply.github.com>
2019-09-28 01:59:30 -0700

    Add files via upload
    Need to display lyrics to song with minimal redundancy using static methods and println

---
+++ Song.java
@@ -0,0 +1,97 @@
+// Berk Uzuncaova
+// 9/7/2019
+// CSE142
+// TA: Jun Song
+// Assignment #1
+//
+// This program will display the lyrics to the song, "There Was an Old Woman Who Swallowed a Fly.”
+
+public class Song {
+   public static void main(String[] args){
+      System.out.println("There was an old woman who swallowed a fly.");
+      dontKnowWhy();
+      perhapsDie();
+      
+      System.out.println("");
+      
+      System.out.println("There was an old woman who swallowed a spider,");
+      System.out.println("That wriggled and iggled and jiggled inside her.");
+      catchTheFly();
+      dontKnowWhy();
+      perhapsDie();
+      
+      System.out.println("");
+      
+      System.out.println("There was an old woman who swallowed a bird,");
+      System.out.println("How absurd to swallow a bird.");
+      catchTheSpider();
+      catchTheFly();
+      dontKnowWhy();
+      perhapsDie();
+      
+      System.out.println("");
+
+      System.out.println("There was an old woman who swallowed a cat,");
+      System.out.println("Imagine that to swallow a cat.");
+      catchTheBird();
+      catchTheSpider();
+      catchTheFly();
+      dontKnowWhy();
+      perhapsDie();
+      
+      System.out.println("");
+      
+      System.out.println("There was an old woman who swallowed a dog,");
+      System.out.println("What a hog to swallow a dog.");
+      catchTheCat();
+      catchTheBird();
+      catchTheSpider();
+      catchTheFly();
+      dontKnowWhy();
+      perhapsDie();
+
+      System.out.println("");
+      
+      System.out.println("There was an old woman who swallowed a bear,");
+      System.out.println("What a big scare to swallow a bear.");
+      System.out.println("She swallowed the bear to catch the dog,");
+      catchTheCat();
+      catchTheBird();
+      catchTheSpider();
+      catchTheFly();
+      dontKnowWhy();
+      perhapsDie();
+
+      System.out.println("");
+      
+      System.out.println("There was an old woman who swallowed a horse,");
+      System.out.println("She died of course.");
+
+   } 
+ 
+   public static void perhapsDie() {
+      System.out.println("Perhaps she'll die.");
+   }   
+   
+   public static void dontKnowWhy() {
+      System.out.println("I don't know why she swallowed that fly,");  
+   }
+   
+   public static void catchTheFly() {
+      System.out.println("She swallowed the spider to catch the fly,");
+   }
+   
+   public static void catchTheSpider() {
+      System.out.println("She swallowed the bird to catch the spider,");
+   }
+   
+   public static void catchTheBird() {
+      System.out.println("She swallowed the cat to catch the bird,");
+   }
+   
+   public static void catchTheCat() {
+      System.out.println("She swallowed the dog to catch the cat,");
+   }
+}   
+           
+   
